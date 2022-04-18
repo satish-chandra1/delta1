e92724156a8d115461f84d6a42d0783a071316ff
satish-chandra1 <satishpathak936@gmail.com>
2022-04-18 21:38:32 +0530

    Array code push

---
+++ javaaa/src/practicealll/ArrayNumberduplicate.java
@@ -0,0 +1,16 @@
+package practicealll;
+
+public class ArrayNumberduplicate {
+
+	public static void main(String[] args) {
+		int []a= {1,2,3,4,4,4,5,5};
+		for(int i=0;i<a.length;i++) {
+			for(int j=1;j<a.length;j++) {
+				
+				
+			}
+		}
+
+	}
+
+}
